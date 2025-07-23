096305b8bb1fe1f8c4149c40e903bb333c338bfb
yht-tutong <yaohaotong111@163.com>
2025-07-23 10:21:57 +0800

    First commit
    First commit

---
+++ src/main/java/com/tutong/welcome/ServiceWelcomeSystemClient.java
@@ -0,0 +1,10 @@
+package com.tutong.welcome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServiceWelcomeSystemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/tutong/welcome/ServiceWelcomeSystemDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tutong.welcome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ServiceWelcomeSystemDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
